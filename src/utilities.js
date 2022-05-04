f677cae8de49b050be4910d017b6ba934fac1f4e
Sankar Battula <sankar.battula2@gmail.com>
2022-05-05 03:51:59 +0530

    React Object detection

---
+++ src/utilities.js
@@ -0,0 +1,22 @@
+export const drawRect = (detections, ctx) =>{
+  // Loop through each prediction
+  detections.forEach(prediction => {
+
+    // Extract boxes and classes
+    const [x, y, width, height] = prediction['bbox']; 
+    const text = prediction['class']; 
+    const score = prediction['score'];
+    const textScore = text + " " + Number.parseFloat(score * 100).toFixed(0) + "%";
+    // Set styling
+    const color = Math.floor(Math.random()*16777215).toString(16);
+    ctx.strokeStyle = '#' + color
+    ctx.font = '18px Arial';
+
+    // Draw rectangles and text
+    ctx.beginPath();   
+    ctx.fillStyle = '#' + color
+    ctx.fillText(textScore, x, y);
+    ctx.rect(x, y, width, height); 
+    ctx.stroke();
+  });
+}
